222fd78bf1ae8042237acd952804f5c34d1ede02
Muhammad Shoaib <omuhammadisaifi@gmail.com>
2019-12-02 16:29:36 +0500

    Initial commit

---
+++ app/src/main/java/com/example/landscape_mode/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.landscape_mode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        /*
+        * Landscape is very easy to createable.
+        * Open design of main.xml.
+        * Now go to landscape option and click on more.
+        * Select the orientation and slct landscape mode.
+        * Now just design it according to layout.
+        * Nothing else to do it will automatic work.
+        * in res file landscape main.xml will be automatically created.*/
+
+    }
+}
